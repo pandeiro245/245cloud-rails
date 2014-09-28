79afda5f0c2d72486c986c671ae41707a3d7ef45
Hironobu Nishikokura <nishiko@mindia.jp>
2014-09-28 12:21:26 +0900

    User.refresh

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ParseUser
-  def workload_count date
-    #TODO
-    Workload.where(created_at: date.to_date).where(is_done: true).count
+  fields :workload_total_count
+
+  def self.refresh
+    User.all.each{|u| u.update_workload_count}
+  end
+
+  def update_workload_count
+    self.workload_total_count = self.workload_count
+    self.save
+  end
+
+  def workload_count
+    Workload.where(is_done: true, user: self).count
+  end
+
+  def best_music
+    Workload.limit(10000).where(user: self, is_done: true).map{|w| w.attributes['sc_id'].to_i > 0 ? "soundcloud:#{w.attributes['sc_id'].to_i.to_s}" : "youtube:#{w.attributes['yt_id']}"}.group_by(&:to_s).map(&:first)
   end
 end
