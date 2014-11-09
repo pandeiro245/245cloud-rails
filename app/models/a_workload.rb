8ed4135cb0e1e74dd6126ccc01ced3828ce2860e
Hironobu Nishikokura <nishiko@mindia.jp>
2014-11-10 01:02:47 +0900

    [WIP] update ranking

--- app/models/a_workload.rb
+++ app/models/a_workload.rb
@@ -2,27 +2,38 @@ class AWorkload < ActiveRecord::Base
   self.table_name = "workloads"
 
   def self.ranking(date = nil)
-    date = '2014-09-04'
-    next_date = (date.to_date + 1.day).to_s
     users = {}
+    html = ""
+    limit = 10
+
+    if date
+      limit = 20
+      next_date = (date.to_date + 1.day).to_s
+      cond = "created_at > '#{date} 00:00:00' and created_at <= '#{next_date} 00:00:00'"
+    else
+      cond = ""
+    end
     
     AWorkload.where(
-      "created_at > '#{date} 00:00:00'"
-    ).where(
-      "created_at <= '#{next_date} 00:00:00'"
+      cond
     ).each do |w|
       users[w.user_hash] = 0 unless users[w.user_hash]
       users[w.user_hash] += 1
     end
-    
+  
+    i = 0
     users.to_a.sort{|a, b| b[1] <=> a[1]}.each do |u|
+      next if i > limit #FIXME
       user = User.find(u[0])
-      p u[0]
-      p "#{user.attributes['name']}: #{u[1]}回"
+      p "#{user.attributes['name']} #{user.attributes['objectId']}:#{u[1]}"
+      html += "<img src='#{user.attributes['icon_url']}' style='max-height: 50px;' />: #{u[1]}回　"
+      i += 1
     end
+    return html
   end
 
   def self.sync!
+    AWorkload.delete_all
     Workload.limit(10000000).where(is_done: true).each do |w|
       begin
         a = AWorkload.find_or_create_by(
